5c8c05b582ccefc089e09e1cfc08ce746aa215db
mak7an <mmmm.mm@mail.com>
2024-10-26 21:01:27 +0300

    Create app.vue

---
+++ app.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+import { Component, Vue, Setup, toNative } from "vue-facing-decorator"
+
+@Component({
+  // async asyncData() {
+  //   await new Promise((resolve) => {
+  //     setTimeout(resolve, 10)
+  //   })
+  //   return { value: 42 }
+  // },
+})
+class PlaygroundApp extends Vue {
+  value!: number
+
+  @Setup(() => definePageMeta({
+    layout: 'admin',
+  }))
+  layout!: never
+
+  inc() {
+    this.value++
+  }
+}
+
+export default toNative(PlaygroundApp)
+</script>
+
+<template>
+</template>
